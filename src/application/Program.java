a0bb98add16366a532dda2cb0026a59cd036175e
AlexandreAntonaci <antonacci25@yahoo.com.br>
2020-12-15 15:31:17 -0300

    Projeto modificado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String aluno;
+		double idade;
+		double serie;
+		
+		System.out.print("Digite o nome do aluno   :");
+		aluno = sc.next();
+		System.out.print("Digite a idade do aluno  :");
+		idade = sc.nextDouble();
+		System.out.print("Digite a série           :");
+		serie = sc.nextDouble();
+		
+		
+		
 	}
 
 }
